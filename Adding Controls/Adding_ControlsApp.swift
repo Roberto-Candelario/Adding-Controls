1fadb8ea37988b0e6a66d64836390eba072c630e
Roberto Candelario <r-jc@live.com>
2024-08-11 19:25:51 -0400

    Initial Commit

---
+++ Adding Controls/Adding_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adding_ControlsApp.swift
+//  Adding Controls
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Adding_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adding ControlsTests/Adding_ControlsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Adding_ControlsTests.swift
+//  Adding ControlsTests
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import Testing
+@testable import Adding_Controls
+
+struct Adding_ControlsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
