b8994fb045460f937611d625973a470fb4a68697
Roberto Candelario <r-jc@live.com>
2024-08-11 22:50:45 -0400

    Completed UI Elements

---
+++ Adding Controls/ReservationCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationCalendar.swift
+//  Adding Controls
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import SwiftUI
+
+struct ReservationCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationCalendar()
+}
